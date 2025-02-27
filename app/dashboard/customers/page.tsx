f819f49ce4cde1601ac3d2f9159efd69c6d89c55
Marc Brodeur <46334979+mrbrodeur@users.noreply.github.com>
2025-02-27 10:47:39 -0500

    pagination

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,24 @@
-export default function Page() {
-    return <p>Customers Page</p>;
-  }
+import CustomersTable from '@/app/ui/customers/table';
+import { lusitana } from '@/app/ui/fonts';
+import { Suspense } from 'react';
+import { TableRowSkeleton } from '@/app/ui/skeletons';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+ 
+export default async function Page(props:{}) {
+  const poo = 'poo'
+  return (
+    <main>
+      <div className="w-full">
+            <div className="flex w-full items-center justify-between">
+              <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+            </div>
+            <Suspense fallback={<TableRowSkeleton />}>
+              <CustomersTable />
+            </Suspense>
+          </div>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        
+      </div>
+    </main>
+  );
+}
